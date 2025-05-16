dc254d5bfe8b9bd4053fd39ac17b347c486b0385
xinggui666 <3380154240@qq.com>
2025-05-16 20:21:38 +0800

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $pdo;
+    
+    public function __construct() {
+        $host = getenv('DB_HOST');
+        $dbname = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+        
+        $this->pdo = new PDO(
+            "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+    }
+    
+    public function query($sql, $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+}
